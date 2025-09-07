079a06df6ef3de77a5f4b567e1b0989e3c6d271e
Gabriel Guirguis <123414705+GabrielGuirguis@users.noreply.github.com>
2025-09-06 20:01:51 -0400

    Update allowed origins for CORS

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,52 @@
-require('dotenv').config()
+require("dotenv").config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var crimesRouter = require('./routes/crimesRoute')
-var callsRouter = require('./routes/callsRoute')
+var indexRouter = require("./routes/index");
+var crimesRouter = require("./routes/crimesRoute");
+var callsRouter = require("./routes/callsRoute");
 
 var app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}));
+app.use(
+  cors({
+    origin: "https://safe-in-the-six.vercel.app",
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/crimes', crimesRouter);
-app.use('/calls', callsRouter)
+app.use("/", indexRouter);
+app.use("/crimes", crimesRouter);
+app.use("/calls", callsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
